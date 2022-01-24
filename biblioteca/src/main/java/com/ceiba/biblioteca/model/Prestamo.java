99a3c1ddbb43c4f0d1366d21c5dc747cd9ba806d
Luis Fernando Ushiña <guffenix@gmail.com>
2022-01-24 14:08:48 -0500

    getter y setters

--- biblioteca/src/main/java/com/ceiba/biblioteca/model/Prestamo.java
+++ biblioteca/src/main/java/com/ceiba/biblioteca/model/Prestamo.java
@@ -1,2 +1,63 @@
-package com.ceiba.biblioteca.model;public class Prestamo {
+package com.ceiba.biblioteca.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "PRESTAMO")
+public class Prestamo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    Long id;
+    @Column(unique = true, length = 10)
+    String isbn;
+    @Column(name = "identificacion_usuario",length = 10)
+    String identificacionUsuario;
+    @Column(name = "tipo_usuario", nullable = false)
+    Integer tipoUsuario;
+    @Column(name = "fecha_maxima_devolucion", columnDefinition = "DATE")
+    LocalDate fechaMaximaDevolucion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getIdentificacionUsuario() {
+        return identificacionUsuario;
+    }
+
+    public void setIdentificacionUsuario(String identificacionUsuario) {
+        this.identificacionUsuario = identificacionUsuario;
+    }
+
+    public Integer getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(Integer tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public LocalDate getFechaMaximaDevolucion() {
+        return fechaMaximaDevolucion;
+    }
+
+    public void setFechaMaximaDevolucion(LocalDate fechaMaximaDevolucion) {
+        this.fechaMaximaDevolucion = fechaMaximaDevolucion;
+    }
 }
