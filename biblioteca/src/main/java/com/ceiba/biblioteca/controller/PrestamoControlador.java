9f006ab8e703198dfcc5d58eea11f2c1aaf96a74
Luis Fernando Ushiña <guffenix@gmail.com>
2022-01-24 13:49:51 -0500

    odelo pojo prestamo

---
+++ biblioteca/src/main/java/com/ceiba/biblioteca/controller/PrestamoControlador.java
@@ -0,0 +1,70 @@
+package com.ceiba.biblioteca.controller;
+
+
+import com.ceiba.biblioteca.model.dto.MensajeDto;
+import com.ceiba.biblioteca.model.dto.PrestamoDto;
+import com.ceiba.biblioteca.model.dto.RespuestaExistosaDto;
+import com.ceiba.biblioteca.model.service.IPrestamoService;
+import com.ceiba.biblioteca.util.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static com.ceiba.biblioteca.util.TipoUsuarioUtils.*;
+
+@RestController
+public class PrestamoControlador {
+
+    @Autowired
+    IPrestamoService prestamoService;
+
+    @PostMapping("/prestamo")
+    public ResponseEntity<?> prestamo(@RequestBody PrestamoDto prestamoDto){
+
+
+        if(ValidateUser(prestamoDto.getTipoUsuario())){
+
+            Optional<PrestamoDto> result = prestamoService.findByIdentificacionUsuario(prestamoDto.getIdentificacionUsuario());
+
+            if(result.isPresent() && ValidateUserInvitado(prestamoDto.getTipoUsuario())){
+                return new ResponseEntity<>(
+                        new MensajeDto(String.format("El usuario con identificación %s ya tiene un libro prestado por lo cual no se le puede realizar otro préstamo",prestamoDto.getIdentificacionUsuario())),
+                        HttpStatus.BAD_REQUEST);
+            }else{
+
+                try {
+
+                    Optional<PrestamoDto> prestamoByIsbn = prestamoService.findByIsbn(prestamoDto.getIsbn());
+                    if(prestamoByIsbn.isPresent()){
+                        return new ResponseEntity<>(new MensajeDto(String.format("El ISBN %s debería ser un código único",prestamoDto.getIsbn())), HttpStatus.OK);
+                    }else{
+
+                        prestamoDto.setLDFechaMaximaDevolucion(DateUtils.addDaysSkippingWeekends(LocalDate.now(), getDayPerUser(prestamoDto.getTipoUsuario())));
+                        PrestamoDto prestamoDtoTmp = prestamoService.save(prestamoDto);
+
+                        return new ResponseEntity<>(new RespuestaExistosaDto(prestamoDtoTmp.getId(), prestamoDtoTmp.getFechaMaximaDevolucion()), HttpStatus.OK);
+                    }
+
+                }catch (Exception ex){
+                    System.out.println(ex.getMessage());
+                    return new ResponseEntity<>(new MensajeDto("Error"), HttpStatus.BAD_REQUEST);
+                }
+            }
+        }else{
+
+            return new ResponseEntity<>(new MensajeDto("Tipo de usuario no permitido en la biblioteca"), HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @GetMapping("/prestamo/{id-prestamo}")
+    public Optional<PrestamoDto> prestamoById(@PathVariable("id-prestamo") Long idPrestamo){
+        return prestamoService.findById(idPrestamo);
+    }
+
+}
+
---
+++ biblioteca/src/main/java/com/ceiba/biblioteca/model/Prestamo.java
@@ -0,0 +1,2 @@
+package com.ceiba.biblioteca.model;public class Prestamo {
+}
