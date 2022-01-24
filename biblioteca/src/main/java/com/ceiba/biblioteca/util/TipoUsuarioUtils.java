0b68d4c2c21a82b63cf618af8ae4aefb6f2cc45c
Luis Fernando Ushiña <guffenix@gmail.com>
2022-01-24 13:56:40 -0500

    dias para devolucion@

---
+++ biblioteca/src/main/java/com/ceiba/biblioteca/util/TipoUsuarioUtils.java
@@ -0,0 +1,36 @@
+package com.ceiba.biblioteca.util;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import static com.ceiba.biblioteca.util.TipoUsuario.*;
+
+public class TipoUsuarioUtils {
+
+    private static final int  DAYS_USUARIO_AFILIAD0 = 10;
+    private static final int  DAYS_USUARIO_EMPLEADO_BIBLIOTECA = 8;
+    private static final int  DAYS_USUARIO_INVITADO = 7;
+
+    public static int getDayPerUser(Integer codigoTipoUsuario ){
+        int result = 0;
+
+        if(codigoTipoUsuario == USUARIO_AFILIAD0.ordinal()){
+            result = DAYS_USUARIO_AFILIAD0;
+        }
+        if(codigoTipoUsuario == USUARIO_EMPLEADO_BIBLIOTECA.ordinal()){
+            result = DAYS_USUARIO_EMPLEADO_BIBLIOTECA;
+        }
+        if(codigoTipoUsuario == USUARIO_INVITADO.ordinal()){
+            result = DAYS_USUARIO_INVITADO;
+        }
+        return result;
+    }
+
+    public static boolean ValidateUser(Integer codigoTipoUsuario){
+      return Arrays.stream(TipoUsuario.values()).anyMatch(tpuser -> tpuser.ordinal() == codigoTipoUsuario);
+    }
+
+    public static boolean ValidateUserInvitado(Integer codigoTipoUsuario){
+        return  codigoTipoUsuario == USUARIO_INVITADO.ordinal();
+    }
+}
