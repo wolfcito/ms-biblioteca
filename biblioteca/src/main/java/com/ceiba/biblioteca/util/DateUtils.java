06e6efaa2b912c44f0a93bb93ed8074a6bfad1cd
Luis Fernando Ushiña <guffenix@gmail.com>
2022-01-24 14:03:55 -0500

    agregar dias sin contar fin de semana

---
+++ biblioteca/src/main/java/com/ceiba/biblioteca/util/DateUtils.java
@@ -0,0 +1,20 @@
+package com.ceiba.biblioteca.util;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class DateUtils {
+
+    public static LocalDate addDaysSkippingWeekends(LocalDate date, int days) {
+        LocalDate result = date;
+        int addedDays = 0;
+        while (addedDays < days) {
+            result = result.plusDays(1);
+            if (!(result.getDayOfWeek() == DayOfWeek.SATURDAY || result.getDayOfWeek() == DayOfWeek.SUNDAY)) {
+                ++addedDays;
+            }
+        }
+        return result;
+    }
+
+}
